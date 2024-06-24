01a6590e08436c88eebfda9f4a50cd67c5ac51c6
Matthew D'Alonzo <matthew.dalonzo@gm.com>
2024-06-24 13:43:28 -0400

    Remove some expects

--- example-utils/usubscription-static-file/src/lib.rs
+++ example-utils/usubscription-static-file/src/lib.rs
@@ -16,6 +16,7 @@ use std::fs::{self, canonicalize};
 use std::path::PathBuf;
 use std::str::FromStr;
 use up_rust::UUri;
+use serde_json::Value;
 
 pub struct USubscriptionStaticFile {}
 
@@ -35,47 +36,79 @@ impl USubscriptionStaticFile {
         // This is a static file, so we will just return the same set of subscribers
         // for all URIs
         println!("fetch_subscribers for topic: {}", topic);
+        
         let crate_dir = env!("CARGO_MANIFEST_DIR");
         let subscription_json_file = PathBuf::from(crate_dir).join("static-configs/testdata.json");
-        let subscription_json_file = canonicalize(subscription_json_file).ok();
-        println!("subscription_json_file: {subscription_json_file:?}");
-        let data =
-            fs::read_to_string(subscription_json_file.unwrap()).expect("Unable to read file");
-        let res: serde_json::Value = serde_json::from_str(&data).expect("Unable to parse");
 
-        let mut subscribers_map = HashMap::new();
-        for (key, value) in res.as_object().unwrap() {
-            println!("key: {}, value: {}", key, value);
-            let mut subscriber_set: HashSet<UUri> = HashSet::new();
-            for subscriber in value.as_array().unwrap() {
-                println!(
-                    "subscriber: {}",
-                    subscriber.as_str().expect("Unable to parse")
-                );
-                match UUri::from_str(subscriber.as_str().expect("Unable to parse")) {
-                    Ok(uri) => {
-                        println!("All good for subscriber");
-                        subscriber_set.insert(uri);
+        match canonicalize(subscription_json_file) {
+            Ok(subscription_json_file) => {
+                println!("subscription_json_file: {:?}", subscription_json_file);
+                
+                match fs::read_to_string(&subscription_json_file) {
+                    Ok(data) => {
+                        match serde_json::from_str::<Value>(&data) {
+                            Ok(res) => {
+                                let mut subscribers_map = HashMap::new();
+                                
+                                if let Some(obj) = res.as_object() {
+                                    for (key, value) in obj {
+                                        println!("key: {}, value: {}", key, value);
+                                        let mut subscriber_set: HashSet<UUri> = HashSet::new();
+                                        
+                                        if let Some(array) = value.as_array() {
+                                            for subscriber in array {
+                                                println!("subscriber: {}", subscriber);
+                                                
+                                                if let Some(subscriber_str) = subscriber.as_str() {
+                                                    match UUri::from_str(subscriber_str) {
+                                                        Ok(uri) => {
+                                                            println!("All good for subscriber");
+                                                            subscriber_set.insert(uri);
+                                                        }
+                                                        Err(error) => {
+                                                            println!("Error with Deserializing Subscriber: {}", error);
+                                                        }
+                                                    }
+                                                } else {
+                                                    println!("Unable to parse subscriber");
+                                                }
+                                            }
+                                        }
+                                        
+                                        println!("key: {}", key);
+                                        match UUri::from_str(&key.to_string()) {
+                                            Ok(mut uri) => {
+                                                println!("All good for key");
+                                                uri.resource_id = 0x8001;
+                                                subscribers_map.insert(uri, subscriber_set);
+                                            }
+                                            Err(error) => {
+                                                println!("Error with Deserializing Key: {}", error);
+                                            }
+                                        }
+                                    }
+                                }
+                                
+                                println!("{}", res);
+                                dbg!(&subscribers_map);
+                                subscribers_map
+                            }
+                            Err(e) => {
+                                eprintln!("Unable to parse JSON: {}", e);
+                                HashMap::new()
+                            }
+                        }
                     }
-                    Err(error) => {
-                        println!("Error with Deserializing Subscriber: {error}");
+                    Err(e) => {
+                        eprintln!("Unable to read file: {}", e);
+                        HashMap::new()
                     }
-                };
+                }
             }
-            println!("key: {key}");
-            match UUri::from_str(&key.to_string()) {
-                Ok(mut uri) => {
-                    println!("All good for key");
-                    uri.resource_id = 0x8001;
-                    subscribers_map.insert(uri, subscriber_set);
-                }
-                Err(error) => {
-                    println!("Error with Deserializing Key: {error}");
-                }
-            };
+            Err(e) => {
+                eprintln!("Unable to canonicalize path: {}", e);
+                HashMap::new()
+            }
         }
-        println!("{}", res);
-        dbg!(&subscribers_map);
-        subscribers_map
     }
 }
